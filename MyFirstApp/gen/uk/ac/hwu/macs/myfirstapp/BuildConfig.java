7c957fce20fcdf460b39d7ee9e0ae6a3a8420e0f
srinivasancj <srinivasancj@gmail.com>
2014-07-25 17:37:01 +0100

    Initial commit

---
+++ MyFirstApp/gen/uk/ac/hwu/macs/myfirstapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package uk.ac.hwu.macs.myfirstapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MyFirstApp/src/uk/ac/hwu/macs/myfirstapp/MainActivity.java
@@ -0,0 +1,53 @@
+package uk.ac.hwu.macs.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_search:
+            //openSearch();
+            return true;
+        case R.id.action_settings:
+            //openSettings();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+		}
+	}
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+}
